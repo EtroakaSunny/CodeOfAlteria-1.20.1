788e15abf28fe3f7b91360ed5d46b484fb5ee5a7
EtroakaSunny <SonnenKrieger.esch@gmail.com>
2024-12-10 06:46:20 +0100

    Added Event handling to require a ticket and added a used ticket

--- src/main/java/net/dorikku/codeofalteria/CodeOfAlteriaMod.java
+++ src/main/java/net/dorikku/codeofalteria/CodeOfAlteriaMod.java
@@ -4,6 +4,7 @@ import com.mojang.logging.LogUtils;
 import net.dorikku.codeofalteria.item.ModItems;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.PlayerChatMessage;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -20,6 +21,8 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 import java.awt.*;
+
+import static net.minecraft.world.entity.EntityType.PLAYER;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(CodeOfAlteriaMod.MOD_ID)
@@ -46,7 +49,7 @@ public class CodeOfAlteriaMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        
+
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
@@ -58,12 +61,30 @@ public class CodeOfAlteriaMod {
 
     //Subscribe to the event when a user tries to enter the nether. Event happens when user steps into the portal
     @SubscribeEvent
-    public void onPlayerChangeDim(EntityTravelToDimensionEvent event){
-        if(event.getDimension() == Level.NETHER){
-            event.setCanceled(true);
+    public void onPlayerChangeDim(EntityTravelToDimensionEvent event) {
+        //Test if a player is trying
+        if (event.getEntity() instanceof Player && event.getDimension() == Level.NETHER) {
+
+            //Test if the user has a valid ticket
+            if (((Player) event.getEntity()).getInventory().contains(ModItems.VALID_TICKET.get().getDefaultInstance())) {
+
+                //If the user has a valid ticket, replace the ticket with a used ticket
+                for (int i = 0; i < ((Player) event.getEntity()).getInventory().getContainerSize(); i++) {
+                    if (((Player) event.getEntity()).getInventory().getItem(i).getItem() == ModItems.VALID_TICKET.get()) {
+                        ((Player) event.getEntity()).getInventory().removeItem(i, 1);
+                        ((Player) event.getEntity()).getInventory().add(ModItems.USED_TICKET.get().getDefaultInstance());
+                        break;
+                    }
+                }
+                event.setCanceled(false);
+
+            } else {
+                //If the user does not have a valid ticket, cancel the event
+                event.setCanceled(true);
+            }
+
         }
     }
-
 
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
